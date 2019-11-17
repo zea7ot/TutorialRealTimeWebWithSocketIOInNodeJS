4357eeda32e7b922d964b7915adfb7c46b28a6e9
Leon <yanglu@microsoft.com>
2019-11-17 13:05:44 -0800

    tutorial: Completing our tech rooms and challenge
    module: 3.2

--- index.js
+++ index.js
@@ -15,18 +15,26 @@ app.get('/javascript', (req, res) => {
   res.sendFile(__dirname + '/public/javascript.html')
 })
 
+app.get('/swift', (req, res) => {
+  res.sendFile(__dirname + '/public/swift.html')
+})
+
+app.get('/css', (req, res) => {
+  res.sendFile(__dirname + '/public/css.html')
+})
+
 // tech namespace
 const tech = io.of('/tech')
 
 tech.on('connection', socket => {
   socket.on('join', data => {
     socket.join(data.room)
-    tech.in(data.room)
+    tech.in(data.room).emit('message', `New user joined ${data.room} room!`)
   })
 
-  socket.on('message', msg => {
-    console.log(`message: ${msg}`)
-    tech.emit('message', msg)
+  socket.on('message', data => {
+    console.log(`message: ${data.msg}`)
+    tech.in(data.room).emit('message', data.msg)
   })
 
   socket.on('disconnect', () => {
